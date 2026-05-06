f7bbe3d48c16999adb29ed6b803124e277eceab8
ssawa <sw20050719@gmail.com>
2026-05-06 23:41:16 +0900

    debug.hpp に型も表示されるようにした。他のファイルはリセットした

---
+++ debug.hpp
@@ -0,0 +1,109 @@
+#ifndef DEBUG_HPP
+# define DEBUG_HPP
+
+# include <bits/stdc++.h>
+# include <cxxabi.h>
+
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    os << "{";
+    for (int i = 0; i < (int)v.size(); i++) {
+        os << v[i] << (i == (int)v.size() - 1 ? "" : ", ");
+    }
+    return os << "}";
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::set<T> &s) {
+    os << "{";
+    for (auto it = s.begin(); it != s.end(); it++) {
+        os << *it << (std::next(it) == s.end() ? "" : ", ");
+    }
+    return os << "}";
+}
+
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::map<T, U> &m) {
+    os << "{";
+    for (auto it = m.begin(); it != m.end(); it++) {
+        os << it->first << ": " << it->second << (std::next(it) == m.end() ? "" : ", ");
+    }
+    return os << "}";
+}
+
+namespace debug_internal {
+    template <typename T>
+    std::string type_name() {
+        int status;
+        char* demangled = abi::__cxa_demangle(typeid(T).name(), 0, 0, &status);
+        std::string res = (status == 0) ? demangled : typeid(T).name();
+        free(demangled);
+        size_t pos;
+        while ((pos = res.find("std::__cxx11::")) != std::string::npos) res.erase(pos, 14);
+        while ((pos = res.find(", std::allocator<")) != std::string::npos) {
+            int depth = 1;
+            size_t end = pos + 17;
+            while (end < res.size() && depth > 0) {
+                if (res[end] == '<') depth++;
+                if (res[end] == '>') depth--;
+                end++;
+            }
+            res.erase(pos, end - pos);
+        }
+        // Simplify string
+        while ((pos = res.find("basic_string<char, std::char_traits<char>>")) != std::string::npos) {
+            res.replace(pos, 42, "string");
+        }
+        while ((pos = res.find("basic_string<char, std::char_traits<char> >")) != std::string::npos) {
+            res.replace(pos, 43, "string");
+        }
+        // Remove spaces before > to avoid "vector<int >"
+        while ((pos = res.find(" >")) != std::string::npos) res.erase(pos, 1);
+        return res;
+    }
+
+    inline std::vector<std::string> split_names(std::string s) {
+        std::vector<std::string> res;
+        int bracket = 0;
+        std::string cur = "";
+        for (char c : s) {
+            if (c == ',' && bracket == 0) {
+                while (!cur.empty() && cur.back() == ' ') cur.pop_back();
+                res.push_back(cur);
+                cur = "";
+            } else {
+                if (c == '(' || c == '[' || c == '{' || c == '<') bracket++;
+                if (c == ')' || c == ']' || c == '}' || c == '>') bracket--;
+                if (cur.size() || c != ' ') cur += c;
+            }
+        }
+        while (!cur.empty() && cur.back() == ' ') cur.pop_back();
+        res.push_back(cur);
+        return res;
+    }
+
+    inline void debug_out(std::vector<std::string> names, int index) {
+        (void)names; (void)index;
+        std::cerr << "\033[0m" << std::endl;
+    }
+
+    template <typename Head, typename... Tail>
+    inline void debug_out(std::vector<std::string> names, int index, Head H, Tail... T) {
+        if (index > 0) std::cerr << ", ";
+        std::cerr << "\033[36m[" << names[index] << "]\033[0m"
+                  << "\033[33m(" << type_name<Head>() << ")\033[0m"
+                  << " = " << H;
+        debug_out(names, index + 1, T...);
+    }
+}
+
+#define dg(...) \
+    std::cerr << "\033[32m" << "Line " << __LINE__ << ": " << "\033[0m", \
+    debug_internal::debug_out(debug_internal::split_names(#__VA_ARGS__), 0, __VA_ARGS__)
+
+#endif
